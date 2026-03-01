09f338e36f26d65f10ed5c5473305f7c409be41b
waleedilyas721610 <waleedilyas721610@gmail.com>
2026-03-01 15:59:47 +0500

    clean repo - fresh files

---
+++ deploy-commands.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Fake ban a user')
+    .setIntegrationTypes([0, 1]).setContexts([0, 1, 2]),
+
+  new SlashCommandBuilder()
+    .setName('spamraid')
+    .setDescription('Send repeated messages')
+    .addStringOption(o => o.setName('message').setDescription('The message to spam').setRequired(true))
+    .addIntegerOption(o => o.setName('count').setDescription('How many times to send').setRequired(true))
+    .addIntegerOption(o => o.setName('delay').setDescription('Delay in seconds between messages').setRequired(false))
+    .setIntegrationTypes([0, 1]).setContexts([0, 1, 2]),
+
+  new SlashCommandBuilder()
+    .setName('stopspam')
+    .setDescription('Stop the currently running spam')
+    .setIntegrationTypes([0, 1]).setContexts([0, 1, 2]),
+
+  new SlashCommandBuilder()
+    .setName('embedspam')
+    .setDescription('Send repeated embed messages')
+    .addStringOption(o => o.setName('message').setDescription('The message to send with the embed').setRequired(true))
+    .addStringOption(o => o.setName('embed').setDescription('The embed content').setRequired(true))
+    .addIntegerOption(o => o.setName('count').setDescription('How many times to send').setRequired(true))
+    .addIntegerOption(o => o.setName('delay').setDescription('Delay in seconds between messages').setRequired(false))
+    .setIntegrationTypes([0, 1]).setContexts([0, 1, 2]),
+
+  new SlashCommandBuilder()
+    .setName('purge')
+    .setDescription('Delete messages in the current channel')
+    .addIntegerOption(o => o.setName('amount').setDescription('Number of messages to delete').setRequired(true))
+    .addStringOption(o =>
+      o.setName('target').setDescription('Delete everyone\'s messages or just yours').setRequired(true)
+        .addChoices(
+          { name: 'Mine only', value: 'mine' },
+          { name: 'Everyone (requires Manage Messages)', value: 'everyone' }
+        ))
+    .setIntegrationTypes([0, 1]).setContexts([0, 1, 2]),
+
+  new SlashCommandBuilder()
+    .setName('nitro')
+    .setDescription('Send a fake Discord Nitro gift')
+    .setIntegrationTypes([0, 1]).setContexts([0, 1, 2]),
+
+
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Starting command registration...');
+    console.log('');
+    const result = await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+    console.log('Successfully registered ' + result.length + ' commands:');
+    console.log('');
+    result.forEach(cmd => { console.log('  /' + cmd.name + ' registered'); });
+    console.log('');
+    console.log('All done!');
+  } catch (error) {
+    console.error(error);
+  }
+})();
